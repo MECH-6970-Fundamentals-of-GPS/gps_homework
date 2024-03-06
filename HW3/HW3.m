f1bed078ec820c3f38462d77a3dbf0573b637534
Walter Livingston <wblivingston18@gmail.com>
2024-03-05 18:38:05 -0600

    majority of work on HW3

--- HW3/HW3.m
+++ HW3/HW3.m
@@ -1,22 +1,24 @@
 %% PART I
 clear; close all; clc;
 
-Nmc = 1000;             % Number of Monte Carlo Runs
+addpath(genpath('../'))
 
-sigma_a = 0.5;          % Standard Deviation of a
-sigma_b = 0.2;          % Standard Deviation of b
+Nmc = 1000;                 % Number of Monte Carlo Runs
 
-y_ = 0;                 % Expected Mean
+sigma_a = 0.5;              % Standard Deviation of a
+sigma_b = 0.2;              % Standard Deviation of b
+
+y_ = 0;                     % Expected Mean
 sigma_y = sqrt(9*sigma_a^2 ...
-    + 16*sigma_b^2);    % Expected Standard Devation
+    + 16*sigma_b^2);        % Expected Standard Devation
 
 for i = 1:Nmc
     a = sigma_a*randn(1);   % State
     b = sigma_b*randn(1);   % State
     y(i) = 3*a - 4*b;       % Combined State
 end
-y_s = mean(y);           % Sample Mean
-sigma_ys = std(y);       % Sample Standard Deviation
+y_s = mean(y);              % Sample Mean
+sigma_ys = std(y);          % Sample Standard Deviation
 
 fprintf('Sample Mean from Monte Carlo: %0.3g\n', y_s);
 fprintf('Sample Standard Deviation from Monte Carlo: %0.3g\n\n', sigma_ys);
@@ -29,128 +31,317 @@ title('Histogram of Y');
 histogram(y);
 xlabel('Y-Value');
 ylabel('Occurrences');
+ax = gca;
+ax.FontSize = 18;
 
 %% PART II
-clear; close all; clc;
+clear;
 
 Nmc = 1000;
 
-dt = 0.1;               % Time Step [s]
-time = 0:dt:10;         % Time [s]
+dt = 1;                 % Time Step [s]
+time = 0:dt:10*60;      % Time [s]
 sigma1 = 0.1;           % Standard Deviation 1
 sigma2 = 0.01;          % Standard Deviation 2
 
+x1 = zeros(length(time),Nmc);     % Integrated White Noise Vector
+x2 = zeros(length(time),Nmc);     % Integrated White Noise Vector
 for i = 1:Nmc
-    x1 = zeros(length(time),1);     % Integrated White Noise Vector
-    x2 = zeros(length(time),1);     % Integrated White Noise Vector
-    for j = 1:(length(time)-1)
-        x1(j+1) = x1(j) + (sigma1*randn(1))*dt;
-        x2(j+1) = x2(j) + (sigma2*randn(1))*dt;
-    end
-    x1_(i) = mean(x1);
-    sigma_x1(i) = std(x1);
-    x2_(i) = mean(x2);
-    sigma_x2(i) = std(x2);
-
-    if mod(i,250) == 0
-        figure();
-        hold('on');
-        title(['Histogram of MC Run: ',num2str(i)]);
-        histogram(x1);
-        histogram(x2);
-        xlabel('X-Value');
-        ylabel('Occurrences');
+    for j = 2:length(time)
+        x1(j,i) = x1(j-1,i) + (sigma1*randn(1))*dt;
+        x2(j,i) = x2(j-1,i) + (sigma2*randn(1))*dt;
     end
 end
 
-figure();
-hold('on');
-title('Integrated White Noise Mean Comparison');
-plot(1:Nmc, x1_);
-plot(1:Nmc, x2_);
-xlabel('Monte Carlo Run');
-ylabel('Mean (mu)');
-legend('Sigma = 0.1', 'Sigma = 0.01');
+x1_ = mean(x1,2);
+sigma_x1_n = std(x1,1,2);
+x2_ = mean(x2,2);
+sigma_x2_n = std(x2,1,2);
+
+sigma_x1_a = sigma1.*sqrt(time.*dt);
+sigma_x2_a = sigma2.*sqrt(time.*dt);
 
 figure();
 hold('on');
-title('Integrated White Noise Sigma Comparison');
-plot(1:Nmc, sigma_x1);
-plot(1:Nmc, sigma_x2);
-xlabel('Monte Carlo Run');
-ylabel('Standard Deviation (sigma)');
-legend('Sigma = 0.1', 'Sigma = 0.01');
-
-figure();
-hold('on');
-title('Final Monte Carlo Run');
-plot(time,x1);
-plot(time,x2);
+title('Mean of Integrated White Noise vs. Time');
+plot(time, x1_);
+plot(time, x2_);
+yline(0,'k');
 xlabel('Time (s)');
-ylabel('Integrated Noise');
-legend('Sigma = 0.1', 'Sigma = 0.01');
+ylabel('Mean');
+legend('\sigma_w=0.1', '\sigma_w=0.01', 'Zero-Mean');
+ax = gca;
+ax.FontSize = 18;
 
+figure();
+tiledlayout(2,1);
+nexttile();
+hold('on');
+title('Standard Deviation of Integrated Noise with \sigma_w=0.1 vs. Time')
+plot(time, sigma_x1_a);
+plot(time, sigma_x1_n);
+xlabel('Time (s)');
+ylabel('Standard Deviation');
+legend('Analytical', 'Numeric');
+ax = gca;
+ax.FontSize = 18;
+
+nexttile();
+hold('on');
+title('Standard Deviation of Integrated Noise with \sigma_w=0.01 vs. Time')
+plot(time, sigma_x2_a);
+plot(time, sigma_x2_n);
+xlabel('Time (s)');
+ylabel('Standard Deviation');
+legend('Analytical', 'Numeric');
+ax = gca;
+ax.FontSize = 18;
+
+% B)
 tau1 = 1;               % Time Constant [s]
 tau2 = 100;             % Time Constant [s]
 
+x1t1 = zeros(length(time),Nmc);
+x1t2 = zeros(length(time),Nmc);
+x2t1 = zeros(length(time),Nmc);
+x2t2 = zeros(length(time),Nmc);
 for i = 1:Nmc
-    x1t1 = zeros(length(time),1);     % 1st Order GM Vector; sig1; tau1
-    x1t2 = zeros(length(time),1);     % 1st Order GM Vector; sig1; tau2
-    x2t1 = zeros(length(time),1);     % 1st Order GM Vector; sig2; tau1
-    x2t2 = zeros(length(time),1);     % 1st Order GM Vector; sig2; tau2
     for j = 1:length(time)-1
-        x1t1_dot = -(1/tau1)*x1t1(j) + sigma1*randn(1);
-        x1t2_dot = -(1/tau2)*x1t2(j) + sigma1*randn(1);
-        x2t1_dot = -(1/tau1)*x2t1(j) + sigma2*randn(1);
-        x2t2_dot = -(1/tau2)*x2t2(j) + sigma2*randn(1);
+        x1t1_dot = -(1/tau1)*x1t1(j,i) + sigma1*randn(1);
+        x1t2_dot = -(1/tau2)*x1t2(j,i) + sigma1*randn(1);
+        x2t1_dot = -(1/tau1)*x2t1(j,i) + sigma2*randn(1);
+        x2t2_dot = -(1/tau2)*x2t2(j,i) + sigma2*randn(1);
         
-        x1t1(j+1) = x1t1(j) + x1t1_dot*dt;
-        x1t2(j+1) = x1t2(j) + x1t2_dot*dt;
-        x2t1(j+1) = x2t1(j) + x2t1_dot*dt;
-        x2t2(j+1) = x2t2(j) + x2t2_dot*dt;
+        x1t1(j+1,i) = x1t1(j,i) + x1t1_dot*dt;
+        x1t2(j+1,i) = x1t2(j,i) + x1t2_dot*dt;
+        x2t1(j+1,i) = x2t1(j,i) + x2t1_dot*dt;
+        x2t2(j+1,i) = x2t2(j,i) + x2t2_dot*dt;
     end
-    x1t1_(i) = mean(x1t1);
-    sigma_x1t1(i) = std(x1t1);
-    x1t2_(i) = mean(x1t2);
-    sigma_x1t2(i) = std(x1t2);
-    x2t1_(i) = mean(x2t1);
-    sigma_x2t1(i) = std(x2t1);
-    x2t2_(i) = mean(x2t2);
-    sigma_x2t2(i) = std(x2t2);
 end
 
-figure();
-hold('on');
-title('1st Order Gauss Markov Mean Comparison');
-plot(1:Nmc, x1t1_);
-plot(1:Nmc, x1t2_);
-plot(1:Nmc, x2t1_);
-plot(1:Nmc, x2t2_);
-xlabel('Monte Carlo Run');
-ylabel('Mean (mu)');
-legend('Sigma = 0.1; Tau = 1', 'Sigma = 0.1; Tau = 100', ...
-    'Sigma = 0.01; Tau = 1', 'Sigma = 0.01; Tau = 100');
+x1t1_ = mean(x1t1,2);
+sigma_x1t1_n = std(x1t1,1,2);
+x1t2_ = mean(x1t2,2);
+sigma_x1t2_n = std(x1t2,1,2);
+x2t1_ = mean(x2t1,2);
+sigma_x2t1_n = std(x2t1,1,2);
+x2t2_ = mean(x2t2,2);
+sigma_x2t2_n = std(x2t2,1,2);
+
+At1 = (1 - (dt/tau1));
+At2 = (1 - (dt/tau2));
+
+sigma_x1t1_a = sigma1*dt.*sqrt((At1.^(2*time) - 1)./(At1^2 - 1));
+sigma_x1t2_a = sigma1*dt.*sqrt((At2.^(2*time) - 1)./(At2^2 - 1));
+sigma_x2t1_a = sigma2*dt.*sqrt((At1.^(2*time) - 1)./(At1^2 - 1));
+sigma_x2t2_a = sigma2*dt.*sqrt((At2.^(2*time) - 1)./(At2^2 - 1));
 
 figure();
 hold('on');
-title('1st Order Gauss Markov Sigma Comparison');
-plot(1:Nmc, sigma_x1t1);
-plot(1:Nmc, sigma_x1t2);
-plot(1:Nmc, sigma_x2t1);
-plot(1:Nmc, sigma_x2t2);
-xlabel('Monte Carlo Run');
-ylabel('Standard Deviation (sigma)');
-legend('Sigma = 0.1; Tau = 1', 'Sigma = 0.1; Tau = 100', ...
-    'Sigma = 0.01; Tau = 1', 'Sigma = 0.01; Tau = 100');
+title('Mean of 1st Order Guas-Markov Process vs. Time');
+plot(time, x1t1_);
+plot(time, x1t2_);
+plot(time, x2t1_);
+plot(time, x2t2_);
+yline(0,'k')
+xlabel('Time (s)');
+ylabel('Mean');
+legend('\sigma_w=0.1; \tau=1', '\sigma_w=0.1; \tau=100', ['\sigma_w=0.01; ' ...
+    '\tau=1'], '\sigma_w=0.01; \tau=100', 'Zero-Mean');
+ax = gca;
+ax.FontSize = 18;
+
+figure();
+tiledlayout(2,1);
+nexttile();
+hold('on');
+title('Standard Deviation of 1st Order Gauss-Markov Process with \sigma_w=0.1 vs. Time')
+plot(time, sigma_x1t1_a);
+plot(time, sigma_x1t1_n);
+plot(time, sigma_x1t2_a);
+plot(time, sigma_x1t2_n);
+xlabel('Time (s)');
+ylabel('Standard Deviation');
+legend('Analytical; \tau=1', 'Numeric; \tau=1', 'Analytical; \tau=100', 'Numeric; \tau=100');
+ax = gca;
+ax.FontSize = 18;
+
+nexttile();
+hold('on');
+title('Standard Deviation of 1st Order Gauss-Markov Process with \sigma_w=0.01 vs. Time')
+plot(time, sigma_x2t1_a);
+plot(time, sigma_x2t1_n);
+plot(time, sigma_x2t2_a);
+plot(time, sigma_x2t2_n);
+xlabel('Time (s)');
+ylabel('Standard Deviation');
+legend('Analytical; \tau=1', 'Numeric; \tau=1', 'Analytical; \tau=100', 'Numeric; \tau=100');
+ax = gca;
+ax.FontSize = 18;
 
 %% PART V
-clear; close all; clc;
+clear; clc;
 
 Xs = [  0 300;
       100 400;
       700 400;
       800 300];
-BS = [400 0];
 Xu = [401 0];
+Xr = [400 0];
 
-rBu = norm(BS - Xu);
+r_ur = norm(Xu - Xr);
+rho_u = vecnorm(Xs - Xu, 2, 2);
+rho_r = vecnorm(Xs - Xr, 2, 2);
+
+% 2 SVs
+dX = 1e3*ones(1,2);
+Xu_2SVs = zeros(1,2);
+while norm(dX) > 1e-4
+    r = vecnorm((Xs(1:2,:) - Xu_2SVs), 2 ,2);
+    U = (Xs(1:2,:) - Xu_2SVs)./r;
+    H = -U;
+    dX = pinv(H)*(rho_u(1:2) - r);
+    Xu_2SVs = Xu_2SVs + dX';
+end
+
+DOP_2SVs = inv(H'*H);
+PDOP_2SVs = sqrt(diag(DOP_2SVs(1:2,1:2)));
+% 
+% % 4 SVs
+% dX = 1e3*ones(1,2);
+% Xu_4SVs = zeros(1,2);
+% while norm(dX) > 1e-4
+%     r = vecnorm((Xs - Xu_4SVs), 2 ,2);
+%     U = (Xs - Xu_4SVs)./r;
+%     H = -U;
+%     dX = pinv(H)*(rho_u - r);
+%     Xu_4SVs = Xu_4SVs + dX';
+% end
+
+DOP_4SVs = inv(H'*H);
+PDOP_4SVs = sqrt(diag(DOP_4SVs(1:2,1:2)));
+
+% 4 SVs + Clock Bias
+dX = 1e3*ones(1,3);
+Xu_4SVs_bias = zeros(1,3);
+while norm(dX) > 1e-4
+    r = vecnorm((Xs - Xu_4SVs_bias(1:2)), 2 ,2);
+    U = (Xs - Xu_4SVs_bias(1:2))./r;
+    H = [-U ones(length(U),1)];
+    rho_hat = r + Xu_4SVs_bias(3);
+    dX = pinv(H)*(rho_u - rho_hat);
+    Xu_4SVs_bias = Xu_4SVs_bias + dX';
+end
+
+DOP_4SVs_bias = inv(H'*H);
+PDOP_4SVs_bias = sqrt(diag(DOP_4SVs_bias(1:2,1:2)));
+
+% DGPS Single Difference
+rho_ru = rho_r - rho_u;
+r = vecnorm((Xs - Xr(1:2)), 2 ,2);
+U = (Xs - Xr(1:2))./r;
+H = [-U ones(length(U),1)];
+Xru_DGPS1 = pinv(H)*(rho_ru);
+
+DOP_DGPS1 = inv(H'*H);
+PDOP_DGPS1 = sqrt(diag(DOP_DGPS1(1:2,1:2)));
+
+% DGPS Double Difference
+rho_ru = rho_r - rho_u;
+rho_ru_double = diff(rho_ru);
+r = vecnorm((Xs - Xr(1:2)), 2 ,2);
+U = (Xs - Xr(1:2))./r;
+U = diff(U);
+H = -U;
+Xru_DGPS2 = pinv(H)*(rho_ru_double);
+
+DOP_DGPS2 = inv(H'*H);
+PDOP_DGPS2 = sqrt(diag(DOP_DGPS2(1:2,1:2)));
+
+%% PART VI
+% Load Data
+addpath(genpath("./"));
+data = load(filename).RCVR_S1;
+ephem = data.ephem;
+psrL1 = data.measurements.L1.psr;
+dopL1 = data.measurements.L1.doppler;
+psrVarL1 = data.measurements.L1.psr_variance;
+psrL2 = data.measurements.L2.psr;
+time = data.GPS_time.seconds;
+
+filename = 'RCVR_S1_data.mat';
+
+%% PART VII
+clear;
+
+prn4 = genCA(4,1023);
+figure();
+hold('on');
+plot(prn4(1:16), '-x');
+plot(prn4(end-16:end), '-o');
+
+prn4_2046 = genCA(4,2046);
+prn4_1023_1 = prn4_2046(1:1023);
+prn4_1023_2 = prn4_2046(1024:2046);
+corr4 = scorr(prn4_1023_1, prn4_1023_2);
+
+figure();
+plot(corr4);
+
+prn7 = genCA(7);
+figure();
+hold('on');
+plot(prn7(1:16), '-x');
+plot(prn7(end-16:end), '-o');
+
+%% PART VIII
+clear;
+
+prn4 = genCA(4);
+prn7 = genCA(7);
+
+figure();
+histogram(prn4);
+figure();
+histogram(prn7);
+
+figure();
+plot(abs(fft(prn4)));
+figure();
+plot(abs(fft(prn7)));
+
+figure();
+plot(scorr(prn4))
+figure();
+plot(scorr(prn7))
+
+figure()
+plot(scorr(prn4,prn7))
+
+%% PART IX
+clear;
+
+fL1 = 1575.42;
+SR = 10*(1/fL1);
+time = 1:SR:60*1;
+carrL1 = sin(2*pi*fL1*time);
+
+prn4 = genCA(4);
+prn7 = genCA(7);
+
+prn4(prn4 == 0) = -1;
+prn7(prn7 == 0) = -1;
+
+prn4 = upsample(prn4,length(time));
+prn7 = upsample(prn7,length(time));
+
+prn4_carrL1 = prn4.*carrL1;
+prn7_carrL1 = prn7.*carrL1;
+
+figure();
+plot(abs(fftshift(fft(prn4_carrL1))));
+set(gca, 'YScale', 'log');
+figure();
+plot(abs(fftshift(fft(prn7_carrL1))));
+set(gca, 'YScale', 'log');
+
